7eb78d454f8e5ea9c499a41cdc6f9f0d26befa36
Aashiqsingh <aashiqsingh56789@gmail.com>
2025-05-14 10:32:26 +0530

    array method

---
+++ array/array1.js
@@ -0,0 +1,73 @@
+// var arr = [12,"ram",true,12.55] 
+
+var users = ["rajvi","ram","seeta","geeta","bhuwan","hanuman"];
+
+// var x = users.includes("seet")
+// console.log(x);
+
+
+// var x = users.reverse()
+// console.log(x);
+
+// var x = users.slice(2,5)
+
+// console.log(x);
+
+// var x = users.sort()
+// console.log(x);
+
+// var x = users.join(" ")
+// var x = users.join("$")
+// var x = users.join("")
+
+
+// console.log(x);
+
+
+
+
+
+
+
+
+// users.splice(2,0,"mallika")
+// users.splice(2,1,"mallika")
+// users.splice(2,3,"mallika")
+// users.splice(2,1,"mallika","shaini")
+
+
+// console.log(users);
+
+
+
+
+
+// console.log(users);
+// console.log(users.length);
+
+// console.log(users[0]);
+
+// for(let i=0;i<users.length;i++){
+//     console.log(users[i].toUpperCase());
+    
+// }
+
+// var x = users.push("mallika","shaini")
+// console.log(x);
+
+// users.pop()
+
+
+// console.log(users);
+
+
+// users.unshift("mallika")
+
+// console.log(users);
+
+// users.shift()
+
+
+// console.log(users);
+
+
